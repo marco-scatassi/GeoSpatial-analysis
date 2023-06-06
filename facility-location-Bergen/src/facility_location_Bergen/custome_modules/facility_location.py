5d22b7a44ac8f535448ab04722ffa429ddd81600
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2023-06-06 16:04:03 +0200

    Update facility_location.py

--- facility-location-Bergen/src/facility_location_Bergen/custome_modules/facility_location.py
+++ facility-location-Bergen/src/facility_location_Bergen/custome_modules/facility_location.py
@@ -14,7 +14,7 @@ import geopandas as gpd
 from pyomo.environ import *
 import pyomo.environ as pyo
 import matplotlib.pyplot as plt
-from .log import print_INFO_message_timestamp, print_INFO_message
+from log import print_INFO_message_timestamp, print_INFO_message
 
 
 ## Class to create an adjacency matrix from a list of coordinates

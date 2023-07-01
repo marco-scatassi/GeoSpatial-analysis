a96dcc2b4bc78d01d3cb9b73f40bd8ad1f26f876
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2023-07-01 22:25:21 +0200

    Update streamlit_deploy.py

--- facility-location-Bergen/streamlit_deploy.py
+++ facility-location-Bergen/streamlit_deploy.py
@@ -131,7 +131,8 @@ def deterministic_generate_viz(session_state, TIMES, facilities_number):
             content = f.read()
 
         with col2:
-            st.write("\n" * 4)
+            st.write("")
+            st.write("")
             st.markdown(content)
         
         col1, col2 = st.columns(2)

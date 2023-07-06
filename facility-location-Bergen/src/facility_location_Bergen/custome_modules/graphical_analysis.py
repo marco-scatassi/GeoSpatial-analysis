e96fbf35ef5499925985e4b222063a2027fb4ef8
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2023-07-06 11:16:40 +0200

    Update graphical_analysis.py

--- facility-location-Bergen/src/facility_location_Bergen/custome_modules/graphical_analysis.py
+++ facility-location-Bergen/src/facility_location_Bergen/custome_modules/graphical_analysis.py
@@ -94,6 +94,8 @@ def facilities_on_map(fls, extra_text=None, title_pad_l=50):
             is_s = True
     colors = ["red", "black", "blue", "purple", "green", "orange", "pink", "brown", "black", "grey"]
     colors_mapping = {k: colors[i] for i, k in enumerate(mapping.keys())}
+    geojd = {"type": "FeatureCollection"}
+    geojd['features'] = []
     size = 10
     for k, fl in mapping.items():
         c=colors_mapping[k]
@@ -115,8 +117,6 @@ def facilities_on_map(fls, extra_text=None, title_pad_l=50):
                 showlegend=True,
             ))
 
-        geojd = {"type": "FeatureCollection"}
-        geojd['features'] = []
         for lon, lat in zip(rand_jitter(lons[k]), rand_jitter(lats[k])):
             b = control_polygon(lon, lat, width=0.24, height= 0.22) #The width and height of a location pin 
                                                                     # are chosen by trial and error
@@ -126,15 +126,14 @@ def facilities_on_map(fls, extra_text=None, title_pad_l=50):
                                                     "coordinates": bez }})
                                 
                                 
-        layers=[dict(sourcetype='geojson',
+    layers=[dict(sourcetype='geojson',
                     source=geojd,
                     below=' ', 
                     type='fill',   
                     color = c,
                     opacity=0.9
-        )]
+    )]
 
-        fig.update_layout(mapbox_layers =layers)
 
     if is_s:
         title = "deterministic and stochastic solution comparison"
@@ -145,6 +144,7 @@ def facilities_on_map(fls, extra_text=None, title_pad_l=50):
                         mapbox=dict(
                             style="open-street-map",
                             center=dict(lat=fls[0].coordinates.geometry.y.mean(), lon=fls[0].coordinates.geometry.x.mean()),
+                            layers=layers,
                             zoom=9
                             ),
                         legend=dict(

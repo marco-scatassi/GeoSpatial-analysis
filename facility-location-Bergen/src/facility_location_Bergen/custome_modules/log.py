837330f571d359b086b4e256d4b44e1e7a756ddf
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2023-08-04 12:01:12 +0200

    Add files via upload

--- facility-location-Bergen/src/facility_location_Bergen/custome_modules/log.py
+++ facility-location-Bergen/src/facility_location_Bergen/custome_modules/log.py
@@ -1,12 +1,22 @@
 from datetime import datetime
 
 # print INFO message with timestamp
-def print_INFO_message_timestamp(message: str):
+def print_INFO_message_timestamp(message: str, log_file: str = None):
     now = datetime.now()
     timestamp = "[" + now.strftime("%m/%d/%y %H:%M:%S") + "]"
-    print(f"{timestamp} INFO     {message}")
+    output = f"{timestamp} INFO     {message}"
+    if log_file == None:
+        print(output)
+    else:
+        with open(log_file, "a") as f:
+            f.write(output + "\n")
 
 
 # print INFO message
-def print_INFO_message(message: str):
-    print(f"                    INFO     {message}")
+def print_INFO_message(message: str, log_file: str = None):
+    output = f"                    INFO     {message}"
+    if log_file == None:
+        print(output)
+    else:
+        with open(log_file, "a") as f:
+            f.write(output + "\n")

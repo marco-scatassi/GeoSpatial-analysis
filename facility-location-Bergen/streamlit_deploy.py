853121fd4b4be55942bf144518372867b1c6e17b
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2023-08-04 09:11:46 +0200

    Update streamlit_deploy.py

--- facility-location-Bergen/streamlit_deploy.py
+++ facility-location-Bergen/streamlit_deploy.py
@@ -21,7 +21,7 @@ from kedro.pipeline import Pipeline, pipeline
 from kedro.framework.session import KedroSession
 from kedro.framework.project import find_pipelines
 from kedro.framework.startup import bootstrap_project
-##from log import print_INFO_message_timestamp, print_INFO_message
+from log import print_INFO_message_timestamp, print_INFO_message
 from facility_location import (
     FacilityLocation, 
     FacilityLocationReport,

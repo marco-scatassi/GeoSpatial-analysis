d98b805a78a5ccde1f63464f6f6587e289cf954b
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2023-08-29 17:15:37 +0200

    Update streamlit_deploy.py

--- facility-location-Bergen/streamlit_deploy.py
+++ facility-location-Bergen/streamlit_deploy.py
@@ -272,12 +272,12 @@ def graph_manipulation(session_state, TIMES):
             session_state["button_load"] = False
             session_state["stop_and_clear"] = False
         
-            if session_state["button_load"]:
-                placeholder.warning("Process interrupted (load the data to start again)", icon="❌")
-            elif session_state["stop_and_clear"]:
-                placeholder.warning("Process interrupted and state cleared (load the data to start again)", icon="❌")
-            else:
-                placeholder.success("Process completed: changes has been saved. Download data using the download button", icon="✅")
+        if session_state["button_load"]:
+            placeholder.warning("Process interrupted (load the data to start again)", icon="❌")
+        elif session_state["stop_and_clear"]:
+            placeholder.warning("Process interrupted and state cleared (load the data to start again)", icon="❌")
+        else:
+            placeholder.success("Process completed: changes has been saved. Download data using the download button", icon="✅")
 
     ############################################## REFINE GRAPH ############################################## 
     if button_refine:

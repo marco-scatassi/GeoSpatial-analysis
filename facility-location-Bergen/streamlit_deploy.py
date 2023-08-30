35e8ecb17831551ea207941097918ddc7251d8c8
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2023-08-30 14:45:43 +0200

    Update streamlit_deploy.py

--- facility-location-Bergen/streamlit_deploy.py
+++ facility-location-Bergen/streamlit_deploy.py
@@ -115,8 +115,7 @@ def clear_log_files():
                 </html>""")
     
 def stop_and_clear_callback():
-    keys = ["node", "modified_graph", "history_changes", 
-            "node_mapping", "predecessors_id", "successors_id", 
+    keys = ["node", "node_mapping", "predecessors_id", "successors_id", 
             "stop_and_clear", "button_load", "is_form1_disabled", "is_form2_disabled"]
     st.session_state["stop_and_clear"] = True
     st.session_state["button_load"] = False
@@ -307,6 +306,7 @@ def graph_manipulation(session_state, TIMES):
 
     ############################################## REFINE GRAPH ############################################## 
     if "is_submitted" in session_state["refine_graph"].keys():
+        stop_and_clear_callback()
         if session_state["refine_graph"]["is_submitted"]:
             with placeholder:
                 graph_col, _, form_col = st.columns([2,0.25,1])

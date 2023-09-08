aa9c600d7dbb8992c191adb5d29862cd7a9e345f
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2023-09-08 17:03:31 +0200

    Update graphical_analysis.py

--- facility-location-Bergen/src/facility_location_Bergen/custome_modules/graphical_analysis.py
+++ facility-location-Bergen/src/facility_location_Bergen/custome_modules/graphical_analysis.py
@@ -140,7 +140,7 @@ def facilities_on_map(fls, extra_text=None, title_pad_l=50):
         geojd = {"type": "FeatureCollection"}
         geojd['features'] = []
         for lon, lat in zip(rand_jitter(lons[k]), rand_jitter(lats[k])):
-            b = control_polygon(lon, lat, width=0.015, height= 0.010) #The width and height of a location pin 
+            b = control_polygon(lon, lat, width=0.015, height= 0.012) #The width and height of a location pin 
                                                                     # are chosen by trial and error
             bez = BezierCv(b, nr=30)
 

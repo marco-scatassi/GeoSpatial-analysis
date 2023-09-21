e91be8d163e203dac4e2e0f212d3ff2acf1ce5b0
Marco Scatassi <96434607+marco-scatassi@users.noreply.github.com>
2023-09-21 18:23:06 +0200

    Update streamlit_deploy.py

--- facility-location-Bergen/streamlit_deploy.py
+++ facility-location-Bergen/streamlit_deploy.py
@@ -47,7 +47,7 @@ metadata = bootstrap_project(project_path)
 
 TIMES = ["all_day_free_flow", "all_day", "morning", "midday", "afternoon"]
 FACILITIES_NUMBER = [1,2,3]
-HANDPICKED = True
+HANDPICKED = False
 
 # LOG_FILE_PATH = r"/mount/src/geospatial-analysis/facility-location-Bergen/logs/split_roads.log"
 # LOG_FILE_PATH2 = r"/mount/src/geospatial-analysis/facility-location-Bergen/logs/split_roads_changes.log"
